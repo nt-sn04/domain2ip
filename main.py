87009118204c835288019d79270484c32d1497fc
djumanov <djumanovdev@gmail.com>
2026-01-31 19:10:03 +0500

    done

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import socket
+import json
 
 
 def get_ip_by_domain(domain: str) -> str:
-    pass
+    return socket.gethostbyname(domain)
 
 
 def main() -> None:
-    pass
+    with open('domains.json') as jsonfile:
+        data = json.loads(jsonfile.read())
+
+    for domain in data['domains']:
+        data['ips'].append({
+            'domain': domain,
+            'ip': get_ip_by_domain(domain)
+        })
+
+    with open('domains.json', 'w') as jsonfile:
+        jsonfile.write(json.dumps(data, indent=4))
 
 main()
